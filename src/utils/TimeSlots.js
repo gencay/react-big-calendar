0a9baf2718ec3c2f4849e74861b3dca520b7e5dc
Ali Gencay <gencay.ali@hotmail.com>
2019-06-28 14:53:53 -0700

    changing the way gaps are added to the meeting events

--- src/utils/TimeSlots.js
+++ src/utils/TimeSlots.js
@@ -129,13 +129,13 @@ export function getSlotMetrics({ min: start, max: end, step, timeslots }) {
       rangeEnd = dates.min(end, dates.max(start, rangeEnd))
 
       // add gap between time and the event slot
-      let mimicStart = rangeStart
-      let mimicEnd = rangeEnd
+      let mimicStart = new Date(rangeStart.getTime())
+      let mimicEnd = new Date(rangeEnd.getTime())
       mimicStart.setTime(rangeStart.getTime() + 1000 * 60 * 5)
       mimicEnd.setTime(rangeEnd.getTime() - 1000 * 60 * 5)
 
-      const rangeStartMin = positionFromDate(mimicStart)
-      const rangeEndMin = positionFromDate(mimicEnd)
+      const rangeStartMin = positionFromDate(rangeStart)
+      const rangeEndMin = positionFromDate(rangeEnd)
       const top = (rangeStartMin / (step * numSlots)) * 100
 
       return {
